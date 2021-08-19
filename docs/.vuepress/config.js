176ad54b0a6e9745189993fd719f8f699c74336f
Yang <380815323@qq.com>
2021-08-19 23:48:55 +0800

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Chen\'s blog',
     description: '我的个人网站',
+    dest:'dist',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
